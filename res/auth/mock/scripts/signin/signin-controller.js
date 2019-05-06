e8d880bf07b24e5343e720d3cb3ab9538b31c49d
Jia <deepjia@gmail.com>
2019-05-06 16:30:44 +0800

    Update signin-controller.js

--- res/auth/mock/scripts/signin/signin-controller.js
+++ res/auth/mock/scripts/signin/signin-controller.js
@@ -3,34 +3,47 @@ module.exports = function SignInCtrl($scope, $http) {
   $scope.error = null
 
   $scope.submit = function() {
-    var data = {
-      name: $scope.signin.username.$modelValue
-      , email: $scope.signin.email.$modelValue
+    var name =  $scope.signin.username.$modelValue;
+    var password = $scope.signin.email.$modelValue;
+    var mail="abc@126.com";
+    if(true)
+    {
+      var canLogin = false;
+      var data = {
+        name: $scope.signin.username.$modelValue
+        , email: mail
+        , password: $scope.signin.email.$modelValue
+      }
+      $http.post('/auth/api/v1/mock', data)
+        .success(function(response) {
+          console.log(response.isLogin)
+          if(response.isLogin==true){
+            canLogin = true;
+            mail=response.email;
+            //
+            if(canLogin==true){             
+              var data = {
+                name: $scope.signin.username.$modelValue
+                , email: mail
+                , password: $scope.signin.email.$modelValue
+              }
+              $http.post('/auth/api/v1/mock', data)
+                .success(function(response) {
+                  console.log(mail);
+                  location.replace(response.redirect)
+                })
+                .error(function(response) {
+                })
+            }
+            //
+          }
+          else{
+            alert("账号或密码错误，请联系系统管理员");
+          }
+        })
+        .error(function(response) {
+          alert("账号或密码错误，请联系系统管理员");
+        })
     }
-    $scope.invalid = false
-    $http.post('/auth/api/v1/mock', data)
-      .success(function(response) {
-        $scope.error = null
-        location.replace(response.redirect)
-      })
-      .error(function(response) {
-        switch (response.error) {
-          case 'ValidationError':
-            $scope.error = {
-              $invalid: true
-            }
-            break
-          case 'InvalidCredentialsError':
-            $scope.error = {
-              $incorrect: true
-            }
-            break
-          default:
-            $scope.error = {
-              $server: true
-            }
-            break
-        }
-      })
   }
 }
